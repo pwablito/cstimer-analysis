0e3511761fc56559fad0e6039716fc9ce525aecd
Spencer, Paul Whiting <pwablito@gmail.com>
2023-10-30 16:31:00 -0400

    index by date

--- cstimer.py
+++ cstimer.py
@@ -2,6 +2,7 @@ import json
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime as dt
 
 
 def add_aon_lines(df, n, num_std=1):
@@ -16,7 +17,8 @@ with open(sys.argv[1], "r") as f:
     data = json.load(f)
 data_3x3 = data["session1"]
 times_3x3 = [row[0][1] / 1000 for row in data_3x3]
-df = pd.DataFrame({"times": times_3x3})
-add_aon_lines(df, 50)
+dates_3x3 = [dt.fromtimestamp(row[-1]) for row in data_3x3]
+df = pd.DataFrame({"times": times_3x3}, index=dates_3x3)
+add_aon_lines(df, 100)
 df.plot()
 plt.show()
