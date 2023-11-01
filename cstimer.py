22e9c9f3adfd1667827a83c870492b2c72f9cd83
Spencer, Paul Whiting <pwablito@gmail.com>
2023-11-01 15:55:58 -0400

    wrap with main func

--- cstimer.py
+++ cstimer.py
@@ -13,12 +13,17 @@ def add_aon_lines(df, n, num_std=1):
     df[f"ao{n} - {num_std} std"] = mean_line - std_values
 
 
-with open(sys.argv[1], "r") as f:
-    data = json.load(f)
-data_3x3 = data["session1"]
-times_3x3 = [row[0][1] / 1000 for row in data_3x3]
-dates_3x3 = [dt.fromtimestamp(row[-1]) for row in data_3x3]
-df = pd.DataFrame({"times": times_3x3}, index=dates_3x3)
-add_aon_lines(df, 100)
-df.plot()
-plt.show()
+def main():
+    with open(sys.argv[1], "r") as f:
+        data = json.load(f)
+    data_3x3 = data["session1"]
+    times_3x3 = [row[0][1] / 1000 for row in data_3x3]
+    dates_3x3 = [dt.fromtimestamp(row[-1]) for row in data_3x3]
+    df = pd.DataFrame({"times": times_3x3}, index=dates_3x3)
+    add_aon_lines(df, 100)
+    df.plot()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
