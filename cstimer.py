163b894a519dcbce7da8b6f68faf63e653549f46
Spencer, Paul Whiting <pwablito@gmail.com>
2023-11-10 09:58:51 -0500

    Process all sessions

--- cstimer.py
+++ cstimer.py
@@ -16,13 +16,17 @@ def add_aon_lines(df, n, num_std=1):
 def main():
     with open(sys.argv[1], "r") as f:
         data = json.load(f)
-    data_3x3 = data[sys.argv[1]]
-    times_3x3 = [row[0][1] / 1000 for row in data_3x3]
-    dates_3x3 = [dt.fromtimestamp(row[-1]) for row in data_3x3]
-    df = pd.DataFrame({"times": times_3x3}, index=dates_3x3)
-    add_aon_lines(df, 100)
-    df.plot()
-    plt.show()
+    sessions = {f"session{sess_num}": sess_props["name"] for sess_num, sess_props in json.loads(data["properties"]["sessionData"]).items()}
+    for session_id, session_name in sessions.items():
+        data_session = data[session_id]
+        times_session = [row[0][1] / 1000 for row in data_session]
+        dates_session = [dt.fromtimestamp(row[-1]) for row in data_session]
+        df = pd.DataFrame({"times": times_session}, index=dates_session)
+        add_aon_lines(df, 100)
+        df.plot()
+        plt.title(session_name)
+        plt.savefig(f"{session_name}.png")
+        plt.clf()
 
 
 if __name__ == "__main__":
