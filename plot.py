94222111967b2c886b4accbc788498b1b56ac12e
Spencer, Paul Whiting <pwablito@gmail.com>
2023-11-14 22:28:53 -0500

    Fix typo

--- plot.py
+++ plot.py
@@ -14,12 +14,17 @@ def add_aon_lines(df, n, num_std=1):
 
 
 def main():
-    if len(sys.argv != 2):
+    if len(sys.argv) != 2:
         print(f"Usage: {sys.argv[0]} <cstimer_export_file>")
         sys.exit(1)
     with open(sys.argv[1], "r") as f:
         data = json.load(f)
-    sessions = {f"session{sess_num}": sess_props["name"] for sess_num, sess_props in json.loads(data["properties"]["sessionData"]).items()}
+    sessions = {
+        f"session{sess_num}": sess_props["name"]
+        for sess_num, sess_props in json.loads(
+            data["properties"]["sessionData"]
+        ).items()
+    }
     for session_id, session_name in sessions.items():
         data_session = data[session_id]
         times_session = [row[0][1] / 1000 for row in data_session]
